2fae566d18d862eedc090f6b317bdb2e5d506773
sezna <ahansen2@trinity.edu>
2016-02-01 10:38:27 -0600

    rustfmt

--- src/kdtree.rs
+++ src/kdtree.rs
@@ -12,7 +12,7 @@ impl Dimension {
             &Dimension::X => return "X",
             &Dimension::Y => return "Y",
             &Dimension::Z => return "Z",
-            _             => return "Null",
+            _ => return "Null",
         };
     }
 }
@@ -28,62 +28,62 @@ pub struct Particle {
     pub mass: f64,
 }
 impl Particle {
-/*    pub fn clone(&self) -> Particle {
-        return Particle {
-            vx: self.vx,
-            vy: self.vy,
-            vz: self.vz,
-            x: self.x,
-            y: self.y,
-            z: self.z,
-        };
-    }*/
+    //    pub fn clone(&self) -> Particle {
+    // return Particle {
+    // vx: self.vx,
+    // vy: self.vy,
+    // vz: self.vz,
+    // x: self.x,
+    // y: self.y,
+    // z: self.z,
+    // };
+    // }
     pub fn random_particle() -> Particle {
         let rng = rand::thread_rng();
         return Particle {
             vx: rand::random::<f64>(),
             vy: rand::random::<f64>(),
             vz: rand::random::<f64>(),
-            x:  rand::random::<f64>(),
-            y:  rand::random::<f64>(),
-            z:  rand::random::<f64>(),
+            x: rand::random::<f64>(),
+            y: rand::random::<f64>(),
+            z: rand::random::<f64>(),
             radius: rand::random::<f64>(),
             mass: rand::random::<f64>(),
-        }
+        };
 
     }
 }
 #[derive(Clone)]
 pub struct Node {
-    split_dimension: Dimension,             // Dimension that this node splits at.
-    split_value:     f64,                   // Value that this node splits at.
-    left:            Option<Box<Node>>,     // Left subtree.
-    right:           Option<Box<Node>>,     // Right subtree.
-    points:          Option<Vec<Particle>>, // Vector of the points if this node is a Leaf.
-    center_of_mass:  (f64, f64, f64),       // The center of mass for this node and it's children all together. (x, y, z).
-    total_mass:      f64,                   // Total mass of all particles under this node.
-    r_max:           f64,                   // Maximum radius that is a child of this node.
+    split_dimension: Dimension, // Dimension that this node splits at.
+    split_value: f64, // Value that this node splits at.
+    left: Option<Box<Node>>, // Left subtree.
+    right: Option<Box<Node>>, // Right subtree.
+    points: Option<Vec<Particle>>, // Vector of the points if this node is a Leaf.
+    center_of_mass: (f64, f64, f64), /* The center of mass for this node and it's children all together. (x, y, z). */
+    total_mass: f64, // Total mass of all particles under this node.
+    r_max: f64, // Maximum radius that is a child of this node.
 }
 impl Node {
     fn new() -> Node {
         return Node {
-        split_dimension: Dimension::Null,
-        split_value: 0.0,
-        left: None,
-        right: None,
-        points: None,
-        center_of_mass: (0.0, 0.0, 0.0),
-        total_mass: 0.0,
-        r_max: 0.0
-        }
+            split_dimension: Dimension::Null,
+            split_value: 0.0,
+            left: None,
+            right: None,
+            points: None,
+            center_of_mass: (0.0, 0.0, 0.0),
+            total_mass: 0.0,
+            r_max: 0.0,
+        };
     }
-    
-    
+
+
     pub fn display_tree(&self) {
         let mut to_display = Node::display_tree_helper(self, 0);
         to_display.sort_by(|a, b| (a.2).cmp(&b.2));
-        let mut to_display_string:String = "".to_string();
-        let mut prev:i32 = -1;
+        let mut to_display_string: String = "".to_string();
+        let mut prev: i32 = -1;
         for element in to_display {
             let info = format!("split on: {}{}    ", element.0.as_string(), element.1);
             println!("info: {}\n", info);
@@ -96,28 +96,27 @@ impl Node {
         println!("{}", to_display_string);
     }
     // Thank you Steve Klabnik for your help with this function.
-        fn display_tree_helper(node: &Node, level: i32) -> Vec<(Dimension, f64, i32)> {
+    fn display_tree_helper(node: &Node, level: i32) -> Vec<(Dimension, f64, i32)> {
         let dim = node.split_dimension.clone();
         let split_val = node.split_value;
-        let mut to_return:Vec<(Dimension, f64, i32)> = vec![(dim, split_val, level)];
+        let mut to_return: Vec<(Dimension, f64, i32)> = vec![(dim, split_val, level)];
         match node.left {
             Some(ref node) => {
-                let mut tmp_vec =  Node::display_tree_helper(node, level + 1);
+                let mut tmp_vec = Node::display_tree_helper(node, level + 1);
                 to_return.append(&mut tmp_vec);
-            },
+            }
             None => (),
         }
-        
+
         match node.right {
             Some(ref node) => {
-                let mut tmp_vec =  Node::display_tree_helper(node, level + 1);
+                let mut tmp_vec = Node::display_tree_helper(node, level + 1);
                 to_return.append(&mut tmp_vec);
-            },
+            }
             None => (),
         }
         to_return
     }
-
 }
 pub struct KDTree {
     root: Node, // The root Node.
@@ -166,8 +165,9 @@ fn new_root_node(mut pts: Vec<Particle>, max_pts: i32) -> Node {
             count = count + 1;
         }
         // TODO weight center of mass by actual mass
-        root_node.center_of_mass = (x_total / count as f64, y_total / count as f64, z_total / count
-                                    as f64);
+        root_node.center_of_mass = (x_total / count as f64,
+                                    y_total / count as f64,
+                                    z_total / count as f64);
         root_node.total_mass = total_mass;
         root_node.r_max = max_radius;
         root_node.points = Some(pts);
@@ -180,7 +180,7 @@ fn new_root_node(mut pts: Vec<Particle>, max_pts: i32) -> Node {
     } else {
         let mut root_node = Node::new();
         let split_index;
-        let mid = (start + end) / 2 as usize; 
+        let mid = (start + end) / 2 as usize;
         if zdistance > ydistance && zdistance > xdistance {
             // "If the z distance is the greatest"
             // split on Z
@@ -208,14 +208,52 @@ fn new_root_node(mut pts: Vec<Particle>, max_pts: i32) -> Node {
         root_node.left = Some(Box::new(new_root_node(pts, max_pts)));
         root_node.right = Some(Box::new(new_root_node(upper_vec, max_pts)));
         // The center of mass is a recursive definition. This finds the average COM for each node.
-        let center_of_mass_x = (root_node.left.as_ref().expect("unexpected null node #1 ").center_of_mass.0 + root_node.right.as_ref().expect("unexpected null node #4").center_of_mass.0) / 2.0;
-        let center_of_mass_y = (root_node.left.as_ref().expect("unexpected null node #2").center_of_mass.1 + root_node.right.as_ref().expect("unexpected null node #5").center_of_mass.1) / 2.0;
-        let center_of_mass_z = (root_node.left.as_ref().expect("unexpected null node #3").center_of_mass.2 + root_node.right.as_ref().expect("unexpected null node #6").center_of_mass.2) / 2.0;
+        let center_of_mass_x = (root_node.left
+                                         .as_ref()
+                                         .expect("unexpected null node #1 ")
+                                         .center_of_mass
+                                         .0 +
+                                root_node.right
+                                         .as_ref()
+                                         .expect("unexpected null node #4")
+                                         .center_of_mass
+                                         .0) / 2.0;
+        let center_of_mass_y = (root_node.left
+                                         .as_ref()
+                                         .expect("unexpected null node #2")
+                                         .center_of_mass
+                                         .1 +
+                                root_node.right
+                                         .as_ref()
+                                         .expect("unexpected null node #5")
+                                         .center_of_mass
+                                         .1) / 2.0;
+        let center_of_mass_z = (root_node.left
+                                         .as_ref()
+                                         .expect("unexpected null node #3")
+                                         .center_of_mass
+                                         .2 +
+                                root_node.right
+                                         .as_ref()
+                                         .expect("unexpected null node #6")
+                                         .center_of_mass
+                                         .2) / 2.0;
         root_node.center_of_mass = (center_of_mass_x, center_of_mass_y, center_of_mass_z);
-        root_node.total_mass = root_node.left.as_ref().expect("unexpected null node #7").total_mass + root_node.right.as_ref().expect("unexpected null node #8").total_mass;
+        root_node.total_mass = root_node.left
+                                        .as_ref()
+                                        .expect("unexpected null node #7")
+                                        .total_mass +
+                               root_node.right
+                                        .as_ref()
+                                        .expect("unexpected null node #8")
+                                        .total_mass;
         // TODO refactor the next two lines, as they are a bit ugly
         root_node.r_max = if root_node.left.as_ref().expect("unexpected null node #9").r_max >
-            root_node.right.as_ref().expect("unexpected null node #10").r_max { root_node.left.as_ref().expect("unexpected null node #9").r_max} else  {root_node.right.as_ref().expect("unexpected null node #10").r_max};
+                             root_node.right.as_ref().expect("unexpected null node #10").r_max {
+            root_node.left.as_ref().expect("unexpected null node #9").r_max
+        } else {
+            root_node.right.as_ref().expect("unexpected null node #10").r_max
+        };
         return root_node;
     }
 }
@@ -347,32 +385,51 @@ fn find_median_x(pts: &mut Vec<Particle>, start: usize, end: usize, mid: usize) 
 #[test]
 #[allow(dead_code)]
 fn test_tree() {
-   let rng = rand::thread_rng();
-   let mut vec_that_wants_to_be_a_kdtree:Vec<Particle> = Vec::new();
-   for x in 0..100 {
+    let rng = rand::thread_rng();
+    let mut vec_that_wants_to_be_a_kdtree: Vec<Particle> = Vec::new();
+    for x in 0..100 {
         for y in 0..100 {
             for z in 0..10 {
                 let particle = Particle::random_particle();
                 vec_that_wants_to_be_a_kdtree.push(particle);
             }
         }
-   }
-   let kdtree_test = new_kdtree(vec_that_wants_to_be_a_kdtree, 3);
-   assert!(kdtree_test.number_of_particles == 100000);
-   assert!(kdtree_test.max_points == 3);
-   //kdtree_test.display_tree();
-   println!("testing integrity of the big tree\n");
-   go_to_edges(kdtree_test);
-   let mut smaller_vec:Vec<Particle> = Vec::new();
-   println!("displaying a smaller tree\n");
-            for z in 0..50 {
-                let particle = Particle::random_particle();
-                smaller_vec.push(particle);
-   }
+    }
+    let kdtree_test = new_kdtree(vec_that_wants_to_be_a_kdtree, 3);
+    assert!(kdtree_test.number_of_particles == 100000);
+    assert!(kdtree_test.max_points == 3);
+    // kdtree_test.display_tree();
+    println!("testing integrity of the big tree\n");
+    go_to_edges(kdtree_test);
+    let mut smaller_vec: Vec<Particle> = Vec::new();
+    println!("displaying a smaller tree\n");
+    for z in 0..50 {
+        let particle = Particle::random_particle();
+        smaller_vec.push(particle);
+    }
     let smaller_kdtree = new_kdtree(smaller_vec, 10);
     smaller_kdtree.display_tree();
     // Testing center of mass assignment
-    let vector = vec![Particle{vx: 0.0, vy: 0.0, vz: 0.0, x: 1.0, y: 2.0, z: 3.0, mass: 2.0, radius: 1.0}, Particle{vx: 0.0, vy: 0.0, vz: 0.0, x: 2.0, y: 1.0, z: 3.0, mass: 2.0, radius: 1.0}];
+    let vector = vec![Particle {
+                          vx: 0.0,
+                          vy: 0.0,
+                          vz: 0.0,
+                          x: 1.0,
+                          y: 2.0,
+                          z: 3.0,
+                          mass: 2.0,
+                          radius: 1.0,
+                      },
+                      Particle {
+                          vx: 0.0,
+                          vy: 0.0,
+                          vz: 0.0,
+                          x: 2.0,
+                          y: 1.0,
+                          z: 3.0,
+                          mass: 2.0,
+                          radius: 1.0,
+                      }];
     let center_of_mass_test = new_kdtree(vector, 2);
     assert!(center_of_mass_test.root.center_of_mass == (1.5, 1.5, 3.0));
 }
@@ -395,4 +452,3 @@ fn go_to_edges(kdtree: KDTree) {
     println!("number of nodes on right: {}\n", count_of_nodes);
     assert!(count_of_nodes == 15);
 }
-
