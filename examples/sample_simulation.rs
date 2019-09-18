6cb6a9fdf66e702f4be6067253869b689562a4b8
alex-hansen <ahansen@stackline.com>
2019-09-17 21:41:36 -0700

    Added example using basic Entities

---
+++ examples/sample_simulation.rs
@@ -0,0 +1,18 @@
+extern crate grav_tree;
+use grav_tree::entity::Entity;
+use grav_tree::GravTree;
+
+fn main() {
+    let mut vec_that_wants_to_be_a_kdtree: Vec<Entity> = Vec::new();
+    for _ in 0..10_000 {
+        let entity = Entity::random_entity();
+        vec_that_wants_to_be_a_kdtree.push(entity);
+    }
+
+    let mut test_tree = GravTree::new(&mut vec_that_wants_to_be_a_kdtree);
+
+    for i in 0..20 {
+        println!("time step: {}", i);
+        test_tree = test_tree.time_step();
+    }
+}
