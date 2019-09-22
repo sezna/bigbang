75771eeda209a8c79a6a737e281edad005455822
alex-hansen <ahansen@stackline.com>
2019-09-22 08:53:06 -0700

    tests and benches folders

---
+++ benches/bench.rs
@@ -0,0 +1,117 @@
+#![feature(test)]
+extern crate test;
+use bigbang::{AsEntity, CollisionResult, Entity, GravTree};
+
+#[derive(Clone)]
+struct MyEntity {
+    x: f64,
+    y: f64,
+    z: f64,
+    vx: f64,
+    vy: f64,
+    vz: f64,
+    radius: f64,
+}
+
+impl AsEntity for MyEntity {
+    fn as_entity(&self) -> Entity {
+        return Entity {
+            x: self.x,
+            y: self.y,
+            z: self.z,
+            vx: self.vx,
+            vy: self.vy,
+            vz: self.vz,
+            radius: self.radius,
+            mass: if self.radius < 1. { 0.5 } else { 105. },
+        };
+    }
+
+    fn apply_velocity(&self, collision_result: CollisionResult, time_step: f64) -> Self {
+        let (vx, vy, vz) = collision_result.velocity;
+        let (x, y, z) = collision_result.position;
+        MyEntity {
+            vx,
+            vy,
+            vz,
+            x: x + (vx * time_step),
+            y: y + (vy * time_step),
+            z: z + (vz * time_step),
+            radius: self.radius,
+        }
+    }
+
+    fn set_position(&self, position: (f64, f64, f64)) -> Self {
+        let (x, y, z) = position;
+        MyEntity {
+            x,
+            y,
+            z,
+            vx: self.vx,
+            vy: self.vy,
+            vz: self.vz,
+            radius: self.radius,
+        }
+    }
+}
+
+impl MyEntity {
+    pub fn random_entity() -> MyEntity {
+        MyEntity {
+            vx: 0f64,
+            vy: 0f64,
+            vz: 0f64,
+            x: rand::random::<f64>() * 50f64,
+            y: rand::random::<f64>() * 50f64,
+            z: rand::random::<f64>() * 50f64,
+            radius: rand::random::<f64>() / 10f64,
+        }
+    }
+}
+
+#[bench]
+fn bench_time_step_0125(b: &mut test::Bencher) {
+    let mut vec_that_wants_to_be_a_kdtree: Vec<MyEntity> = Vec::new();
+    for _ in 0..125 {
+        let entity = MyEntity::random_entity();
+        vec_that_wants_to_be_a_kdtree.push(entity);
+    }
+
+    let mut test_tree = GravTree::new(&mut vec_that_wants_to_be_a_kdtree, 0.2);
+    b.iter(|| test_tree = test_tree.time_step())
+}
+
+#[bench]
+fn bench_time_step_1000(b: &mut test::Bencher) {
+    let mut vec_that_wants_to_be_a_kdtree: Vec<MyEntity> = Vec::new();
+    for _ in 0..1000 {
+        let entity = MyEntity::random_entity();
+        vec_that_wants_to_be_a_kdtree.push(entity);
+    }
+
+    let mut test_tree = GravTree::new(&mut vec_that_wants_to_be_a_kdtree, 0.2);
+    b.iter(|| test_tree = test_tree.time_step())
+}
+
+#[bench]
+fn bench_time_step_2000(b: &mut test::Bencher) {
+    let mut vec_that_wants_to_be_a_kdtree: Vec<MyEntity> = Vec::new();
+    for _ in 0..2000 {
+        let entity = MyEntity::random_entity();
+        vec_that_wants_to_be_a_kdtree.push(entity);
+    }
+
+    let mut test_tree = GravTree::new(&mut vec_that_wants_to_be_a_kdtree, 0.2);
+    b.iter(|| test_tree = test_tree.time_step())
+}
+#[bench]
+fn bench_time_step_3500(b: &mut test::Bencher) {
+    let mut vec_that_wants_to_be_a_kdtree: Vec<MyEntity> = Vec::new();
+    for _ in 0..3500 {
+        let entity = MyEntity::random_entity();
+        vec_that_wants_to_be_a_kdtree.push(entity);
+    }
+
+    let mut test_tree = GravTree::new(&mut vec_that_wants_to_be_a_kdtree, 0.2);
+    b.iter(|| test_tree = test_tree.time_step())
+}
