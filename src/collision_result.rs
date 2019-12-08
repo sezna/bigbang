11018e4ad4f16a522458f949e8cdbb5d1654a2c5
Alex <alex@42layers.io>
2019-12-08 00:43:44 -0800

    remove collision_result

--- src/collision_result.rs
+++
@@ -1,5 +0,0 @@
-/// Contains the forces that bigbang has calculated are being exerted on an entity.
-pub struct CollisionResult<'a, T> {
-    /// The particles that are potentially being collided with.
-    pub collisions: Vec<&'a T>,
-}
--- src/simulation_result.rs
+++ src/simulation_result.rs
@@ -1,8 +1,7 @@
-use crate::collision_result::CollisionResult;
 
 pub struct SimulationResult<'a, T> {
     /// The result of the simulation's collision check
     /// just a vector of references to potential collisions
-    pub collision_result: CollisionResult<'a, T>,
+    pub collisions: Vec<&'a T>,
     pub gravitational_acceleration: (f64, f64, f64),
 }
