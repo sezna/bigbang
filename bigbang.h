2f255ac1c9c6f50145625bf33cad8239417abc81
Alex <alex@42layers.io>
2020-01-23 02:17:21 -0800

    update header file for FFI

--- bigbang.h
+++ bigbang.h
@@ -16,10 +16,15 @@ typedef struct Entity {
   double mass;
 } Entity;
 
-void *from_data_file(const char *file_path_buff, double time_step);
-
+/**
+ * The exported FFI version of [[GravTree]]'s `new()` method. Returns a void pointer to the location
+ * in memory where the [[GravTree]] is located. Use this void pointer to tell Rust where to look for
+ * the tree in the other FFI functions.
+ */
 void *new(const Entity *array, int length, double time_step);
 
-void *time_step(void *gravtree_buf);
-
-void write_data_file(const char *file_path_buff, unsigned char *gravtree_buf);
+/**
+ * The exported FFI version of [[GravTree]]'s `time_step()` method. Instead of being a method, it is a
+ * function which takes in a [[GravTree]] (rather, a void pointer to the space where the [[GravTree]] is).
+ */
+void *time_step(void *grav_tree_buf);
